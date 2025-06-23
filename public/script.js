205016f86adb6f2aa9ce2169fb95de56ca6d045b
Harish Kumar <harish15kumar06@gmail.com>
2025-06-23 20:41:08 +0530

    Update: Added functionality to the site.

--- public/script.js
+++ public/script.js
@@ -16,3 +16,33 @@ aboutSection.addEventListener("mouseenter",()=>{
   aboutImage.classList.add('animate');
   contactInfo.classList.add('contact-animate')
 });
+
+
+const skillWrapper = document.querySelector('.skill-wrapper');
+const skillContainer = document.querySelector('.skills-container');
+
+
+const clone = skillWrapper.cloneNode(true);
+clone.id = ''; 
+
+skillWrapper.append(...clone.children);
+
+const originalWidth = skillWrapper.scrollWidth / 2;
+
+skillWrapper.style.setProperty('--scroll-distance', `-${originalWidth}px`);
+
+skillContainer.addEventListener('mouseenter', () => {
+  skillWrapper.classList.add('animate-scroll');
+});
+
+skillContainer.addEventListener('mouseleave', () => {
+  skillWrapper.classList.remove('animate-scroll');
+});
+
+function downloadResume(){
+    const link = document.createElement('a');
+    link.href = './my-resume.pdf';
+    link.download = 'Harish-Kumar-Resume';
+    link.click();
+    alert('Downloading...')
+}
