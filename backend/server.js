e83c6ad150f1879adb4fe7178a9caede2d763d22
Harish Kumar <harish15kumar06@gmail.com>
2025-06-28 16:20:27 +0530

    Fix: Server files fixed

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../Assets')));
 
 // ✅ Serve index.html as root
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
